322ae45baa467b00c9b94b404145f05f087cca9f
Your Name <you@example.com>
2022-12-17 18:15:28 +0300

    наведен порядок

--- src/task-3/bubblesort.php
+++ src/task-3/bubblesort.php
@@ -2,36 +2,28 @@
 /*declare(strict_types=1);
 error_reporting(-1);*/
 
-function bubblesort(array $arr)
+function bubbelSort(array $arr)
 {
     $count = count($arr);
-    if ($count <= 1){
+    if ($count <= 1) {
         return $arr;
     }
-    $is_flag = false;
-    $count_iter = 1;
-    for ($i = 0; $i < $count; ++$i){
-        $is_flag = false;
-        for ($j = $count - 1; $j > $i; --$j){
-            if ($arr[$j] < $arr[$j - 1]){
-                $is_flag = true;
-                $tmp = $arr [$j];
+
+    for ($i = 0; $i < $count; ++$i) {
+        for ($j = ($count - 1); $j > $i; --$j) {
+            if ($arr[$j] < $arr[$j - 1]) {
+                $tmp = $arr[$j];
                 $arr[$j] = $arr[$j - 1];
                 $arr[$j - 1] = $tmp;
-                ++$count_iter;
             }
         }
-        if (!$is_flag){
-            echo 'цикл был запущен ' . $count_iter . ' раз' .PHP_EOL;
-            return $arr;
-        }
     }
-    echo 'цикл был запущен ' . $count_iter . ' раз' .PHP_EOL;;
+
     return $arr;
 }
 
-$arr = [1, 10, -4, - 45, 101, 2, 56, 6, 10, -1000];
-print_r(bubblesort($arr));
+$arr = [1, -10, -4, - 45, 101, 2, 56, 6, 10, -1000];
+print_r(bubbelSort($arr));
 
 $arr_1 = [10, 10, 11, 10, 10];
-print_r(bubblesort($arr_1));
+print_r(bubbelSort($arr_1));
