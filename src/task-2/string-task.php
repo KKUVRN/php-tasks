bb4997c5f316682d830413c91167efdbfcede916
Your Name <you@example.com>
2023-01-07 16:18:21 +0300

    задание 30

--- src/task-2/string-task.php
+++ src/task-2/string-task.php
@@ -8,3 +8,11 @@
 $line = 'php';
 $line = strtoupper($line);
 echo $line;
+
+/**
+ * Задание №30. Дана строка 'PHP'. Сделайте из нее строку 'php'
+ */
+
+$line = 'PHP';
+$line = strtolower($line);
+echo $line;
